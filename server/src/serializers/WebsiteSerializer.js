5c4d02417d65d753c3231feeb17fbfac14d61425
Therese Atalanta Gordon <thereseatalantagordon@Thereses-MacBook-Air.local>
2023-05-05 11:21:06 -0400

    we implement a comment serializer

--- server/src/serializers/WebsiteSerializer.js
+++ server/src/serializers/WebsiteSerializer.js
@@ -1,18 +1,23 @@
+import CommentSerializer from "./CommentSerializer.js"
+
 class WebsiteSerializer {
     static async showDetails(website) {
         const allowedAttributes = ["id", "name", "url", "description"]
-
         let serializedWebsite = {}
         for (const attribute of allowedAttributes) {
             serializedWebsite[attribute] = website[attribute]
         }
-        serializedWebsite.comments = await website.$relatedQuery("comments")
+        const relatedComments = await website.$relatedQuery("comments")
+        const serializedComments = await Promise.all(
+            relatedComments.map(async (comment) =>
+                CommentSerializer.showCommentDetails(comment))
+        )
+        serializedWebsite.comments = serializedComments
         return serializedWebsite
     }
 
     static getDetailsForList(website) {
         const allowedAttributes = ["id", "name", "url", "description"]
-
         let serializedWebsite = {}
         for (const attribute of allowedAttributes) {
             serializedWebsite[attribute] = website[attribute]
