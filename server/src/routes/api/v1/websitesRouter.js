8d17789b79939ffcbc9cf931a2c34434617908f1
RipNasty <det625@gmail.com>
2023-05-10 12:50:28 -0400

    making sure changes went in

--- server/src/routes/api/v1/websitesRouter.js
+++ server/src/routes/api/v1/websitesRouter.js
@@ -50,26 +50,37 @@ websitesRouter.delete("/:id", async (req, res) => {
     }
 })
 
-// websitesRouter.patch("/:id", async (req, res) => {
-//     const { id } = req.params;
-//     console.log(id)
+websitesRouter.get("/:id/edit", async (req, res) => {
+    const { id } = req.params
+    try {
+        const comment = await Comment.query().findById(id)
+        const serializedComment = await CommentSerializer.showCommentDetails(comment)
+        return res.status(200).json({ comment: serializedComment })
+    } catch (error) {
+        return res.status(500).json({ errors: error })
+    }
+})
 
-//     try {
-//     const commentData = req.body.comment;
-//     if (Object.keys(commentData).length === 0) {
-//         return res.status(400).json({ message: "No update data provided" });
-//     }
-//     const comment = await Comment.query().findById(id);
-//     if (!comment) {
-//         return res.status(404).json({ message: "Comment not found" });
-//     }
-//     await Comment.query().findById(id).patch(commentData);
-//     const updatedComment = await Comment.query().findById(id);
-//     return res.status(200).json({ comment: updatedComment });
-//     } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ errors: error });
-//     }
-// });
+websitesRouter.patch("/:id", async (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+
+    try {
+    const commentData = req.body.comment;
+    if (Object.keys(commentData).length === 0) {
+        return res.status(400).json({ message: "No update data provided" });
+    }
+    const comment = await Comment.query().findById(id);
+    if (!comment) {
+        return res.status(404).json({ message: "Comment not found" });
+    }
+    await Comment.query().findById(id).patch(commentData);
+    const updatedComment = await Comment.query().findById(id);
+    return res.status(200).json({ comment: updatedComment });
+    } catch (error) {
+    console.error(error);
+    return res.status(500).json({ errors: error });
+    }
+});
 
 export default websitesRouter;
