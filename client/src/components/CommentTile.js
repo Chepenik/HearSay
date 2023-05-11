59ed957d79a38e9e31ef6ca239948154be440dba
RipNasty <det625@gmail.com>
2023-05-11 15:57:54 -0400

    made it so Admins can edit/delete all comments, in addition to non-admins only being able to edit/delete their own comments

--- client/src/components/CommentTile.js
+++ client/src/components/CommentTile.js
@@ -2,17 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom"
 
 const CommentTile = ({ comment, handleCommentDelete, currentUser}) => {
-  const { id, comment: text, rating } = comment;
+  const { id, comment: text, rating, userId } = comment;
 
   const handleClickDelete = () => {
     handleCommentDelete(id)
   }
 
+  console.log(currentUser)
+
+  const isAdmin = currentUser && currentUser.admin === true
+  const isSameUser = currentUser && currentUser.id === userId
+
   return (
     <div className="comment-tile">
       <p>Rating: {rating}</p>
       <p>{text}</p>
-      {currentUser && currentUser.id === comment.userId && (
+      {(isSameUser || isAdmin) && (
         <> 
         <button type="delete" onClick={handleClickDelete}>Delete</button>
         <br></br>
@@ -22,7 +27,8 @@ const CommentTile = ({ comment, handleCommentDelete, currentUser}) => {
         </>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default CommentTile;
+export default CommentTile
+
